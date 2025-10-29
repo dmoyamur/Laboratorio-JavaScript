a2ad8341060d3bb7040608764d3a78095445a7a1
dmoyamur <dmoya844@gmail.com>
2025-10-29 14:49:11 -0500

    Laboratorio Primitivos JavaScript

---
+++ primitivos.js
@@ -0,0 +1,37 @@
+const prompt = require("prompt-sync")();
+
+"1. VARIABLES Y TIPOS DE DATOS"
+"Ejercicio 1"
+let nombre = "Diana";
+let edad = 42;
+let esEstudiante = true;
+
+"Ejercicio 2"
+let sinValor;
+let valorNulo = null;
+console.log(sinValor);
+console.log(valorNulo);
+
+"2. INGRESOS DE DATOS POR TECLADO"
+"Ejercicio 1"
+let anioNacim = prompt("Escribe tu año de nacimiento: ");
+edad = 2025 - anioNacim;
+console.log("Tu edad es: "+edad+" años");
+
+"Ejercicio 2"
+let numero1 = prompt("Ingresa el primer número: ");
+let numero2 = prompt("Ingresa el segundo número: ");
+let num1 = Number(numero1);
+let num2 = Number(numero2);
+let suma = num1 + num2;
+console.log("La suma de los números "+num1+" y "+num2+" es: "+suma);
+
+"3 CONDICIONALES"
+"Ejercicio 1"
+let edad_usuario = prompt ("Ingresa tu edad: ")
+let edad_usu = Number(edad_usuario)
+if (edad_usu >= 18) {
+    console.log("Puedes entrar")
+} else {
+    console.log("No puedes entrar")
+}
